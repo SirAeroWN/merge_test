e5ff48cc20f45755bb55b21b1f0376615d2c2237
Will Norvelle <wnorvelle@alarm.com>
2019-09-17 10:50:24 -0400

    Poor spelllling in my commit mesage

--- test.js
+++ test.js
@@ -4,12 +4,11 @@
  * @param {number} a
  * @param {number} b
  *
- * @return {number}
  */
 function test(a, b) {
     return a + b;
 }
 
-console.log(test(3, 4))
-test(a,   b);
+console.log(test(3, 4));
+test(a, b);
 console.log(test(4, 5));
